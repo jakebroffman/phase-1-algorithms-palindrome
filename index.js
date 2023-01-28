b8d7c290608f7395216f5fe84467e94d561d2ed7
jakebroffman <jake.broffman1088@gmail.com>
2023-01-28 16:59:24 -0500

    commit tree 5b21e7d724dee95fbce29599399b41964f8cf9ab

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Write your algorithm here 
+ // data being taken in is a string
+ // need to use an if else statement for returning boolean of true or false
+ // take first half of string and compare it to second half... convert to an array?
+
+ const array = word.split('');
+ const reverseArray = array.reverse();
+ const reverseString = reverseArray.join('');
+
+ if(word === reverseString) {
+  return true
+  console.log('It is a palindrome!');
+ }
+ else {
+  return false
+  console.log('It is not a palindrome!')
+ }
+
 }
 
 /* 
   Add your pseudocode here
-*/
+*/ // Take in a string
+  // Convert the string to an array
+  // Compare the array to the reverse of the array
+  // If array === reverseArray return true
+  // else return false
 
 /*
   Add written explanation of your solution here
